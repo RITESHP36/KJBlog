312d05212b32e53ee1cb33edecaa728b532953f2
RITESHP36 <codingritesh36@gmail.com>
2024-02-29 14:41:17 +0530

    removing vercel speeds

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,10 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
     <Analytics />
-    <SpeedInsights />
   </BrowserRouter>
 )
